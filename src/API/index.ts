a2e79ad98f8239310d9b7dce9571288290cade1b
shreyanshsing <shreyanshsinghjee@gmail.com>
2022-06-18 17:52:48 +0530

    fix: replaced heroku url

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const Axios = ({method, url, data, params}: {method: string, url: string, data?: any, params?: any}) => axios({
     method: method,
-    url: `http://localhost:3001/${url}`,
+    url: `https://imparticus.herokuapp.com/${url}`,
     data: data,
     params: params
 })
